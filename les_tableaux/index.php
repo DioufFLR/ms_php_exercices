69d3b1aa912ab5823713d2fd0ae46a0ea43da24f
DioufFLR <geoffreyfleur@gmail.com>
2023-01-04 15:29:03 +0100

    partie 1

--- les_tableaux/index.php
+++ les_tableaux/index.php
@@ -8,6 +8,44 @@
     <title>Les tableaux</title>
 </head>
 <body>
+<h1>Les tableaux</h1>
+<div class="wrapper" align="center">
+    <div class="partie1">
+        <h2>Partie 1</h2>
+
+        <?php
+             $annee = array(
+                     "Janvier" => 31,
+                     "Février" => 28,
+                     "Mars" => 31,
+                     "Avril" => 30,
+                     "Mai" => 31,
+                     "Juin" => 30,
+                     "Juillet" => 31,
+                     "Aout" => 31,
+                     "Septembre" => 30,
+                     "Octobre" => 31,
+                     "Novembre" => 30,
+                     "Décembre" => 31,
+             );
+
+             asort($annee);
+
+             echo '<table>';
+
+             foreach ($annee as $mois => $jours) {
+                 echo '<tr>' . '<td>' . $mois . '</td><td>' . $jours . '</td>' . '</tr>';
+             }
+
+             echo '</table>';
+        ?>
+
+
+
+    </div>
+</div>
+
 
 </body>
 </html>
+
