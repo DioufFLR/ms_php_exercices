bb37054d2dce6d5b90bca9e8d201bf43388831d1
DioufFLR <geoffreyfleur@gmail.com>
2023-01-04 16:49:59 +0100

    partie 2 en cours

--- les_tableaux/index.php
+++ les_tableaux/index.php
@@ -31,7 +31,7 @@
 
              asort($annee);
 
-             echo '<table>';
+             echo '<table border="1.5">';
 
              foreach ($annee as $mois => $jours) {
                  echo '<tr>' . '<td>' . $mois . '</td><td>' . $jours . '</td>' . '</tr>';
@@ -40,7 +40,79 @@
              echo '</table>';
         ?>
 
+    </div>
 
+    <div class="partie2">
+        <h2>Partie 2</h2>
+
+        <?php
+        $capitales = array(
+            "Bucarest" => "Roumanie",
+            "Bruxelles" => "Belgique",
+            "Oslo" => "Norvège",
+            "Ottawa" => "Canada",
+            "Paris" => "France",
+            "Port-au-Prince" => "Haïti",
+            "Port-d'Espagne" => "Trinité-et-Tobago",
+            "Prague" => "République tchèque",
+            "Rabat" => "Maroc",
+            "Riga" => "Lettonie",
+            "Rome" => "Italie",
+            "San José" => "Costa Rica",
+            "Santiago" => "Chili",
+            "Sofia" => "Bulgarie",
+            "Alger" => "Algérie",
+            "Amsterdam" => "Pays-Bas",
+            "Andorre-la-Vieille" => "Andorre",
+            "Asuncion" => "Paraguay",
+            "Athènes" => "Grèce",
+            "Bagdad" => "Irak",
+            "Bamako" => "Mali",
+            "Berlin" => "Allemagne",
+            "Bogota" => "Colombie",
+            "Brasilia" => "Brésil",
+            "Bratislava" => "Slovaquie",
+            "Varsovie" => "Pologne",
+            "Budapest" => "Hongrie",
+            "Le Caire" => "Egypte",
+            "Canberra" => "Australie",
+            "Caracas" => "Venezuela",
+            "Jakarta" => "Indonésie",
+            "Kiev" => "Ukraine",
+            "Kigali" => "Rwanda",
+            "Kingston" => "Jamaïque",
+            "Lima" => "Pérou",
+            "Londres" => "Royaume-Uni",
+            "Madrid" => "Espagne",
+            "Malé" => "Maldives",
+            "Mexico" => "Mexique",
+            "Minsk" => "Biélorussie",
+            "Moscou" => "Russie",
+            "Nairobi" => "Kenya",
+            "New Delhi" => "Inde",
+            "Stockholm" => "Suède",
+            "Téhéran" => "Iran",
+            "Tokyo" => "Japon",
+            "Tunis" => "Tunisie",
+            "Copenhague" => "Danemark",
+            "Dakar" => "Sénégal",
+            "Damas" => "Syrie",
+            "Dublin" => "Irlande",
+            "Erevan" => "Arménie",
+            "La Havane" => "Cuba",
+            "Helsinki" => "Finlande",
+            "Islamabad" => "Pakistan",
+            "Vienne" => "Autriche",
+            "Vilnius" => "Lituanie",
+            "Zagreb" => "Croatie"
+        );
+        
+        sort($capitales);
+        
+        foreach ($capitales as $villes => $pays) {
+            echo $villes . $pays;
+        }
+        ?>
 
     </div>
 </div>
