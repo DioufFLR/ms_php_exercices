b01e2447f4a260f9d37bb79ea4a5d396cf5ba981
DioufFLR <geoffreyfleur@gmail.com>
2023-01-06 11:53:18 +0100

    Les fonctions partie 1

--- les_fonctions/index.php
+++ les_fonctions/index.php
@@ -8,16 +8,43 @@
     <title>Les fonctions</title>
 </head>
 <body>
+<div class="parti1">
+    <h2>Générer un lien</h2>
+    <h3>Première manière</h3>
+    <?php
 
-<?php
 
-function lien($a, $b) {
-    echo '<a href=' . '"' . $a . '"' . '>' . $b . '</a>';
-}
+    function lien($a, $b) {
+        echo '<a href=' . '"' . $a . '"' . '>' . $b . '<a>';
+    }
 
-lien("https://www.reddit.com/", "Reddit Hug");
+    lien("https://www.reddit.com/", "Reddit Hug");
 
-?>
+    ?>
+    <br>
+    <h3>Deuxième manière</h3>
+    <?php
+
+    $c = "<a ";
+    $d = "\">";
+    $e = "</a>";
+    $f = htmlentities($c);
+    $g = htmlentities($d);
+    $h = htmlentities($e);
+
+    function lien2($a, $b) {
+        global $f;
+        global $g;
+        global $h;
+        echo  $h . "href=\"". $a . $g . $b . $h;
+    }
+
+    lien2("https://www.reddit.com/", "Reddit Hug");
+    ?>
+</div>
+
+
+
 
 </body>
 </html>
