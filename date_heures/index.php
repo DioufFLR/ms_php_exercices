bfdd35e5ef988bff12972326e8e342191c51a023
DioufFLR <geoffreyfleur@gmail.com>
2023-01-10 10:03:12 +0100

    Date et heures 1.1

--- date_heures/index.php
+++ date_heures/index.php
@@ -1,8 +1,88 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="partie1">
+        <br>
+        <h2>Partie 1</h2>
+        <?php
 
-echo 'hello world';
+        $date_str = "2022-07-14";
+        $date_time = strtotime($date_str);
+        echo 'Cela correspond à la semaine : ' . date ('W', $date_time);
 
-// À ajouter à chaques fois dans le script avant toute manipulation de dates
-date_default_timezone_set("Europe/Paris");
-echo time();
-echo '<br>' . date("d/m/y");
+        ?>
+        <br>
+    </div>
+
+    <div class="partie2">
+        <br>
+        <h2>Partie 2</h2>
+        <?php
+
+
+
+        ?>
+        <br>
+    </div>
+
+    <div class="partie3">
+        <br>
+        <h2>Partie 3</h2>
+        <?php
+
+
+        ?>
+        <br>
+    </div>
+
+    <div class="partie4">
+        <br>
+        <h2>Partie 4</h2>
+        <?php
+
+
+        ?>
+        <br>
+    </div>
+
+    <div class="partie5">
+        <br>
+        <h2>Partie 5</h2>
+        <?php
+
+
+
+        ?>
+        <br>
+    </div>
+
+    <div class="partie6">
+        <br>
+        <h2>Partie 6</h2>
+        <?php
+
+
+
+        ?>
+        <br>
+    </div>
+
+    <div class="partie7">
+        <br>
+        <h2>Partie 7</h2>
+        <?php
+
+
+        ?>
+        <br>
+    </div>
+</body>
+</html>
+
