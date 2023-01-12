85b8f0bfbe8b542b9e603bb57260af91364a87da
DioufFLR <geoffreyfleur@gmail.com>
2023-01-12 15:37:00 +0100

    formulaires end

--- formulaires_variables_serveur/index.php
+++ formulaires_variables_serveur/index.php
@@ -12,15 +12,53 @@
 <h1>Formulaires et variables serveur</h1>
 
 <div class="form">
-    <form action ="check.php" method="post">
-        Tu utilises internet plutôt le :<br />
-        <input type="checkbox" name="Fjour[]" value="Lundi" />Lundi<br />
-        <input type="checkbox" name="Fjour[]" value="Mardi" />Mardi<br />
-        <input type="checkbox" name="Fjour[]" value="Mercredi" />Mercredi<br />
-        <input type="checkbox" name="Fjour[]" value="Jeudi" />Jeudi<br />
-        <input type="checkbox" name="Fjour[]" value="Vendredi" />Vendredi<br />
-        <input type="submit" name="Envoyer" value="ENVOYER" />
+    <h1 class="title">Contact</h1>
+    <form action="script.php" method="post" id="myForm">
+        <p>* Ces zones sont obligatoires</p>
+        <fieldset>
+            <legend>Vos coordonnées</legend>
+            <label for="nom" >Votre nom* :</label><input type="text" name="nom" id="nom" >
+            <br>
+            <span id="nomManquant"></span>
+            <br>
+            <label for="prenom">Votre prenom* :</label><input type="text" name="prenom" id="prenom">
+            <br>
+            <span id="prenomManquant"></span>
+            <br>
+            <!-- -----Création d'un bouton radio requis avec homme pour valeur par défaut----- -->
+            <div>
+                <p>Sexe* :</p>
+                <input type="radio" name="sexe" id="man" value="Homme" checked>
+                <label for="man">Homme</label>
+                <input type="radio" name="sexe" id="woman" value="Femme">
+                <label for="woman">Femme</label>
+            </div>
+            <label for="birth">Date de naissance* :</label><input type="date" name="birth" id="birth">
+            <br>
+            <span id="dateNaissanceManquant"></span>
+            <br>
+            <label for="cd">Code postal* :</label><input type="text" name="cd" id="cd">
+            <br>
+            <span id="codePostalManquant"></span>
+            <br>
+            <label for="adress">Adresse :</label><input type="text" name="adress" id="adress">
+            <br>
+            <label for="city">Ville :</label><input type="text" name="city" id="city" minlength="1">
+            <br>
+            <label for="email">Email* :</label><input type="text" id="email" name="email">
+            <br>
+            <span id="emailManquant"></span>
+        </fieldset>
+        <label for="check"></label>* J'accepte le traitement informatique de ce formulaire<input type="checkbox" name="check" id="check" required>
+
+
+        <div class="submit">
+            <input type="submit" value="Envoyer" id="boutonEnvoi">
+            <input type="reset" value="Annuler">
+        </div>
     </form>
+    </main>
+
 </div>
 
 
--- formulaires_variables_serveur/script.php
+++ formulaires_variables_serveur/script.php
@@ -1,3 +1,6 @@
 <?php
-/* Récupère le nom et le prénom automatiquement */
-echo "bonjour " . $_REQUEST['prenom'] . " " . $_REQUEST['nom'] . "<br>";
+echo "Bonjour " . $_REQUEST['nom'] . " " . $_REQUEST['prenom'] . ".<br>";
+echo "Vous êtes un(e) " . $_REQUEST['sexe'] . " et vous êtes né(e) le " . $_REQUEST['birth'] . ".<br>";
+echo "Vous habitez au " . $_REQUEST['adress'] . " à " . $_REQUEST['city'] . " " . $_REQUEST['cd'] . ".<br>";
+echo "Votre adresse mail est elle correcte ? " . $_REQUEST['email'] . ".<br>";
+echo "Si toutes ces informations sont correctes veuillez continuer sinon recommencer.";
