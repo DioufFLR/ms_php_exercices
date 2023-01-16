725f4ed3ca5ca8f3c2c8d42a851c37f60e997ef5
DioufFLR <geoffreyfleur@gmail.com>
2023-01-16 10:47:25 +0100

    pdo create read

--- pdo/artists.php
+++ pdo/artists.php
@@ -46,6 +46,25 @@ $requete->closeCursor();
 
     <input type="submit" value="Ajouter">
 
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nom</th>
+            <!-- Ici, on ajoute une colonne pour insérer un lien -->
+            <th></th>
+        </tr>
+
+        <?php foreach ($tableau as $artist): ?>
+            <tr>
+                <td><?= $artist->artist_id ?></td>
+                <td><?= $artist->artist_name ?></td>
+                <!-- Ici, on ajoute un lien par artiste pour accéder à sa fiche : -->
+                <td><a href="artist_detail.php?id=<?= $artist->artist_id ?>">Détail</a></td>
+            </tr>
+        <?php endforeach; ?>
+
+    </table>
+
 </form>
 </body>
 </html>
