72a5421f6de703d008153107ddab8939cfde4f66
DioufFLR <geoffreyfleur@gmail.com>
2023-01-27 10:24:23 +0100

    add les_sessions

--- les_sessions/index.php
+++ les_sessions/index.php
@@ -1,4 +1,17 @@
 <?php
 session_start();
-$_SESSION["login"] = "webmaster";
-echo $_SESSION["login"];
+
+if ($_SESSION["login"]) {
+    echo "Vous êtes autorisé à voir cette page.";
+} else {
+    echo "Cette page nécessite une identification.";
+}
+
+unset($_SESSION["login"]);
+unset($_SESSION["role"]);
+
+if (ini_get("session.use_cookies")) {
+    setcookie(session_name(), '', time()-42000);
+}
+
+session_destroy();
