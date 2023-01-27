8b42baba6cc7ab00e2cac52d6ed35d2ff8dedb63
DioufFLR <geoffreyfleur@gmail.com>
2023-01-27 11:47:47 +0100

    add les_sessions

---
+++ authentification/login_form.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercices sur les mots de passe</title>
+</head>
+<body>
+<div class="form">
+    <form action="login_script.php" method="post">
+
+        <label for="login">login</label><br>
+        <input type="text" name="login" id="login">
+
+        <br><br>
+
+        <label for="password">Mot de passe</label><br>
+        <input type="password" name="password" id="password">
+
+    </form>
+</div>
+</body>
+</html>
