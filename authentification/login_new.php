2ac4c7627b3f04f7452a4836fad0bed80db4543e
DioufFLR <geoffreyfleur@gmail.com>
2023-02-03 11:35:43 +0100

    1.7 voir commentaire login_new.php

--- authentification/login_new.php
+++ authentification/login_new.php
@@ -23,6 +23,8 @@ $requete->closeCursor();
 </head>
 <body>
 
+<!------------------------------https://www.chiny.me/exercice-authentification-via-une-base-de-donnees-en-php-8-13.php voir cette page pour finir l'exercice --------------------------->
+
 <div class="container">
     <div class="p-3 mb-2 bg-info text-white">
         <h1>Inscription</h1>
@@ -53,6 +55,12 @@ $requete->closeCursor();
         <a href="login_form.php" class="btn btn-info">Retour</a>
     </div>
 </div>
-
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
+
+
+
 </html>
+
